7c84abb94e48529890865b78777b0461b6078fcc
ANTHONY SANT ANNA DA SILVA <60251578046@senacrs.edu.br>
2026-07-01 16:20:30 -0300

    mudanças

---
+++ scripts/menu.js
@@ -0,0 +1,10 @@
+ 
+ 
+ 
+const toggleButton = document.querySelector('#container header .toggle');
+
+const headerElement = document.querySelector('#container header')
+
+toggleButton.addEventListener('click', function() {
+    headerElement.classList.toggle('active');
+})
